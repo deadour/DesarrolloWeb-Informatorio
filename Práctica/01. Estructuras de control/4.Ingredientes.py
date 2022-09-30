0ea7681937db4ac0e08b68ec209fa02f0d79a59d
eduramirez87 <edu.ramirez645@gmail.com>
2022-09-29 23:41:57 -0300

    probando

---
+++ Práctica/01. Estructuras de control/4.Ingredientes.py
@@ -0,0 +1,12 @@
+#DESAFÍO 4
+#Tenemos que decidir entre 2 recetas ecológicas.
+#Los ingredientes para cada tipo de receta aparecen a continuación.
+
+#Ingredientes comunes: Verduras y berenjena.
+#Ingredientes Receta 1: Lentejas y apio.
+#Ingredientes Receta 2: Morrón y Cebolla..
+
+#Escribir un programa que pregunte al usuario que tipo de receta desea,
+#y en función de su respuesta le muestre un menú con los ingredientes disponibles para que elija.
+#Solo se puede eligir 3 ingrediente (entre la receta elegida y los comunes.) y el tipo de receta.
+#Al final se debe mostrar por pantalla la receta elegida y todos los ingredientes.
